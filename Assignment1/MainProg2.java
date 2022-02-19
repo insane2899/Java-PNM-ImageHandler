649eebd1231bb39d71f375581ba8d6930366767e
insane2899 <imrimosen@gmail.com>
2022-02-19 12:48:27 +0530

    cleaned a bit more

--- Assignment1/MainProg2.java
+++ Assignment1/MainProg2.java
@@ -9,5 +9,7 @@ public class MainProg2{
 	public static void main(String[] args)throws IOException{
 		PNMImage img = ImageUtil.readImage("Dataset/image2.ppm");
 		ImageUtil.writeImage(img,"Assignment1/color.raw.pnm","P6");
+		img =  ImageUtil.readImage("Dataset/maple_input.pgm");
+		ImageUtil.writeImage(img,"Assignment1/maple_input.pgm","P2");
 	}
 }
